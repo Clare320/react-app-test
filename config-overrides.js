acfb5273e2f3ff427570c63c807ea9973c4a7557
lingjie.li <lingjie.li@weimob.com>
2021-04-22 19:02:52 +0800

    eject cra

--- config-overrides.js
+++ config-overrides.js
@@ -2,27 +2,31 @@ const path = require('path');
 
 module.exports = function override(config, env) {
     //do stuff with the webpack config...
-    let configs = {};
+    let configs = config;
 
-    configs = {
-        entry: './src/index.js',
-        output: {
-            filename: 'bundle.js',
-            path: path.resolve(__dirname, '/dist')
-        },
-        module: {
+    configs.entry = './src/index.js';
+    configs.output = {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '/dist')
+    };
+
+    if (configs.module && configs.module.rules) {
+        configs.module.rules.push([
+            {
+                test: /\.(ts|tsx)$/,
+                use: 'ts-loader'
+            }
+        ]);
+    } else {
+        configs.module = {
             rules: [
-                { 
-                    test: /\.css$/,
-                    use: 'css-loader'
-                },
                 {
-                    test: /\.ts$/,
+                    test: /\.(ts|tsx)$/,
                     use: 'ts-loader'
                 }
             ]
-        }
-    };
+        };
+    }
 
     return configs;
   }
